99ecab8385c6bcced80946bff25c3a31e60ac054
AmirhusseinSoori <amirhusseinsoori75@gmail.com>
2020-12-16 22:21:35 +0330

    Use Header and footer in paging

--- app/src/main/java/com/amirhusseinsoori/imageapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/amirhusseinsoori/imageapp/ui/gallery/GalleryFragment.kt
@@ -26,7 +26,10 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
-            recyclerView.adapter = adapter
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+            )
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
