a3be41e865bc44cbc04aafb109792a680a9437ce
AmirhusseinSoori <amirhusseinsoori75@gmail.com>
2020-12-16 21:07:49 +0330

    Navigation Component

---
+++ app/src/main/java/com/amirhusseinsoori/imageapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.amirhusseinsoori.imageapp.R
+
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
